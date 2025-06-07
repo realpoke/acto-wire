3b896b823dd658f003385dd6f9900ea7ecfd0717
RealPoke <m@rtin.win>
2025-06-07 03:09:38 +0200

    style: Apply automated code style fixes

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,24 +1,23 @@
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
-if (import.meta.env.VITE_BROADCAST_CONNECTION === 'pusher') {
+if (import.meta.env.VITE_BROADCAST_CONNECTION === "pusher") {
     window.Echo = new Echo({
-        broadcaster: 'pusher',
+        broadcaster: "pusher",
         key: import.meta.env.VITE_PUSHER_APP_KEY,
         cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-        forceTLS: true
+        forceTLS: true,
     });
-} else if (import.meta.env.VITE_BROADCAST_CONNECTION === 'reverb') {
+} else if (import.meta.env.VITE_BROADCAST_CONNECTION === "reverb") {
     window.Echo = new Echo({
-        broadcaster: 'reverb',
+        broadcaster: "reverb",
         key: import.meta.env.VITE_REVERB_APP_KEY,
         wsHost: import.meta.env.VITE_REVERB_HOST,
         wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
         wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-        enabledTransports: ['ws', 'wss'],
+        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+        enabledTransports: ["ws", "wss"],
     });
 }
-
